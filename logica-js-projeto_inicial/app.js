1eedf42f0f87c22a6e879869a54922324022acf4
Jotta <jottakayoml@gmail.com>
2023-10-24 17:22:57 -0300

    Iniciando projeto

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,10 @@
+alert("Boas vindas ao jogo Nº Secreto.")//Show the message on the screen.
+let secretNumber = 24;//variable declaration
+let chosenNumber = prompt("Escolhe ai um numero de 1 á 50 !");//Storing the answer from prompt
+
+
+if (secretNumber == chosenNumber ) {
+    alert("Acertou miseravi !")
+}else{
+    alert("errou miseravi !")
+}
