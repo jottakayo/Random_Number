6668635eafda6310c7cd80e8105afd6cff5c4a40
Jotta <jottakayoml@gmail.com>
2023-10-24 23:44:03 -0300

    Adicionando Operador ternário

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,10 +1,33 @@
-alert("Boas vindas ao jogo Nº Secreto.")//Show the message on the screen.
-let secretNumber = 24;//variable declaration
-let chosenNumber = prompt("Escolhe ai um numero de 1 á 50 !");//Storing the answer from prompt
+alert("Boas vindas ao jogo Nº Secreto.")//Alerta de boas vindas.
+let secretNumber = 24;//Guarda o numero secreto.
+console.log(secretNumber);
+let chosenNumber;//Numero escolhido pelo usuário.
+let attempts = 1;//Tentativas do usuário.
 
 
-if (secretNumber == chosenNumber ) {
-    alert("Acertou miseravi !")
-}else{
-    alert("errou miseravi !")
+while(chosenNumber != secretNumber){
+    let chosenNumber = prompt("Escolhe ai um numero de 1 á 50 !");
+    if (chosenNumber == secretNumber ) {
+        break
+    }else{
+        if (chosenNumber > secretNumber) {
+            alert(`errou miseravi, o numero certo é menor que ${chosenNumber} que você escolheu !`)
+        }else{
+            alert(`errou miseravi, o numero certo é maior que ${chosenNumber} que você escolheu !`)
+        }
+        //attempts = attempts + 1;
+        attempts++;//Armazena o numero de tentativas do usuário.
+    }
 }
+//Operador ternário
+let wordAttempts = attempts > 1 ? 'tentativas' : 'tentativa';//Ajusta a palavra tentativa para tentativa caso seja o nº 1.
+
+alert(`Acertou miseravi , você acertou o numero secreto ${secretNumber} com ${attempts} ${wordAttempts} !`);
+
+
+// if (attempts = 1){
+// alert(`Acertou miseravi , você acertou o numero secreto ${secretNumber} com ${attempts} tentativa !`)
+// } else {
+//     alert(`Acertou miseravi , você acertou o numero secreto ${secretNumber} com ${attempts} tentativas !`)
+// }
+
