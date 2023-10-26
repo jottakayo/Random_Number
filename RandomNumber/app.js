a74bacc268fa040a4526fe4bf1aaf1b9ad07621b
Jotta <jottakayoml@gmail.com>
2023-10-26 16:02:26 -0300

    Finalização do projeto incluindo requisições back-end

--- RandomNumber/app.js
+++ RandomNumber/app.js
@@ -1,38 +1,10 @@
-// function startGame() {
-// alert("Boas vindas ao jogo Nº Secreto.")//Alerta de boas vindas
-// let secretNumber = parseInt(Math.random() * 50 + 1);//Gera um numero aleatório com o atributo Math.random
-// console.log(secretNumber);
-// let chosenNumber;//Numero escolhido pelo usuário
-// let attempts = 1;//Tentativas do usuário
-
-// // Loop para até o jogador acerte o numero randômico
-// while(chosenNumber != secretNumber){
-//     let chosenNumber = prompt("Escolha entre 1 e 50.");
-//     // Converte a escolha do jogador para número inteiro
-//         chosenNumber = parseInt(chosenNumber);
-//     if (chosenNumber == secretNumber ) {
-//         break
-//     }else{
-//         if (chosenNumber > secretNumber) {
-//             alert(`Você errou o numero, o numero certo é menor que ${chosenNumber} que você escolheu !`)
-//         }else{
-//             alert(`Você errou o numero, o numero certo é maior que ${chosenNumber} que você escolheu !`)
-//         }
-//         attempts++;//Armazena o numero de tentativas do usuário.
-//     }
-// }
-
-// let wordAttempts = attempts > 1 ? 'tentativas' : 'tentativa';//Ajusta a palavra tentativa para tentativa caso seja o nº 1.
-// // Atualiza o resultado na página HTML
-// document.getElementById("result").textContent = `Você acertou o numero secreto ${secretNumber} com somente ${attempts} ${wordAttempts} !`;
-// }
-
+//Declaração de variáveis globais
+let listNumber = [];
 let secretNumber = generateSecretNumber();
-console.log(secretNumber);
-
 let attempts = 1;
+
 //Função para mudar o texto em html
-function printText(tag, text){//Função com 2 parâmetros para imprimir textos na tag HTML
+function printText(tag, text){
     let camp = document.querySelector(tag);
     camp.innerHTML = text;
 }
@@ -40,44 +12,55 @@ startMessageGame();
 
 //Função que checa o numero gerado pelo usuário e compara com o secreto
 function checkKick () {
-    let kick = document.querySelector('input').value//Método querySelector para buscar valor de inout
+    let kick = document.querySelector('input').value//Método querySelector para buscar valor de input
     console.log(kick == secretNumber);
         //Logica para determinar o numero secreto correto
         if ( kick == secretNumber){
             let wordAttempts = attempts > 1 ? 'tentativas' : 'tentativa';
             printText('h1', 'Você <span class="container__text-blue">acertou</span> !');
-            printText('h2', `Você acertou o numero secreto ${secretNumber} com ${attempts} ${wordAttempts}.`);
-            document.getElementById('reiniciar').removeAttribute('disabled');
+            printText('h2', `Você acertou o numero secreto com ${attempts} ${wordAttempts}.`);
+            document.getElementById('reload').removeAttribute('disabled');
         } else {
+            //If para dar dicas do numero correto
             if ( kick > secretNumber ){ 
-                printText('h1', 'Você errou, tente <span class="container__text-blue">novamente</span> !');
+                printText('h1', 'Tente <span class="container__text-blue">novamente</span> !');
                 printText('h2', `O numero secreto é menor.` )  
         } else {
-            printText('h1', 'Você errou, tente <span class="container__text-blue">novamente</span> !');
+            printText('h1', 'Tente <span class="container__text-blue">novamente</span> !');
             printText('h2', `O numero secreto é maior.`)
         }
-           attempts++;
+        //Contador de tentativas
+        attempts++;
     }
 }
 //Função para gera um numero secreto
 function generateSecretNumber() {
-    return parseInt(Math.random() * 50 + 1);
+    let chosenNumber = parseInt(Math.random() * 50 + 1);
+    //Verificação para que o numero aleatório não seja repetido
+    if (listNumber.includes(chosenNumber)){//includes é um método para verificar se existe ja aquele conteúdo no array, retornando true or false
+        return generateSecretNumber();
+    }else{
+        listNumber.push(chosenNumber);//Push para incluir o numero no array
+        console.log(listNumber);
+        return chosenNumber;
+    }
 }
 //Mensagem inicial do jogo
 function startMessageGame(){
-    printText('h1', 'Jogo do numero <span class="container__text-blue">secreto</span>');
+    printText('h1', 'Jogo do Palpite <span class="container__text-blue">Certo</span>');
     printText('h2', 'Vamos ver como você se sai !.');
 }
-//função para deixa vazio o input do kick
+//função para deixa vazio o input do kick, utilizada na função reloadGame()
 function cleanKick(){
     kick = document.querySelector('input');
     kick.value = ''; 
 }
-//Criar função para reiniciar o jogo
+//Função para reiniciar o jogo
 function reloadGame() {
     secretNumber = generateSecretNumber();
     console.log(secretNumber);
     startMessageGame();
     attempts = 1;
     cleanKick();
+    document.getElementById('reload').setAttribute('disabled', true);//Com esse method(setAttribute) é necessário dar 2 parâmetros, sendo o segundo se você quer o atributo escolhido on or off.
 }
