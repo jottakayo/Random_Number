14a1831a4dd53e6454cb8ab6fe38b13b27b76a0e
Jotta <jottakayoml@gmail.com>
2023-10-27 02:56:59 -0300

    Adicionendo tecnicas usadas no projeto no README.MD

--- RandomNumber/app.js
+++ RandomNumber/app.js
@@ -3,12 +3,46 @@ let listNumber = [];
 let secretNumber = generateSecretNumber();
 let attempts = 1;
 
+startMessageGame();
+
 //Função para mudar o texto em html
 function printText(tag, text){
     let camp = document.querySelector(tag);
     camp.innerHTML = text;
 }
-startMessageGame();
+
+//Mensagem inicial do jogo
+function startMessageGame(){
+    printText('h1', 'Jogo do Palpite <span class="container__text-blue">Certo</span>');
+    printText('h2', 'Vamos ver como você se sai !.');
+}
+//função para deixa vazio o input do kick, utilizada na função reloadGame()
+function cleanKick(){
+    kick = document.querySelector('input');
+    kick.value = ''; 
+}
+//Função para reiniciar o jogo
+function reloadGame() {
+    secretNumber = generateSecretNumber();
+    console.log(secretNumber);
+    startMessageGame();
+    attempts = 1;
+    cleanKick();
+    document.getElementById('reload').setAttribute('disabled', true);//Com esse method(setAttribute) é necessário dar 2 parâmetros, sendo o segundo se você quer o atributo escolhido on or off.
+}
+
+//Função para gera um numero secreto
+function generateSecretNumber() {
+    let chosenNumber = parseInt(Math.random() * 10 + 1);
+    //Verificação para que o numero aleatório não seja repetido
+    if (listNumber.includes(chosenNumber)){//includes é um método para verificar se existe ja aquele conteúdo no array, retornando true or false
+        return generateSecretNumber();
+    }else{
+        listNumber.push(chosenNumber);//Push para incluir o numero no array
+        console.log(listNumber);
+        return chosenNumber;
+    }
+}
 
 //Função que checa o numero gerado pelo usuário e compara com o secreto
 function checkKick () {
@@ -32,35 +66,4 @@ function checkKick () {
         //Contador de tentativas
         attempts++;
     }
-}
-//Função para gera um numero secreto
-function generateSecretNumber() {
-    let chosenNumber = parseInt(Math.random() * 50 + 1);
-    //Verificação para que o numero aleatório não seja repetido
-    if (listNumber.includes(chosenNumber)){//includes é um método para verificar se existe ja aquele conteúdo no array, retornando true or false
-        return generateSecretNumber();
-    }else{
-        listNumber.push(chosenNumber);//Push para incluir o numero no array
-        console.log(listNumber);
-        return chosenNumber;
-    }
-}
-//Mensagem inicial do jogo
-function startMessageGame(){
-    printText('h1', 'Jogo do Palpite <span class="container__text-blue">Certo</span>');
-    printText('h2', 'Vamos ver como você se sai !.');
-}
-//função para deixa vazio o input do kick, utilizada na função reloadGame()
-function cleanKick(){
-    kick = document.querySelector('input');
-    kick.value = ''; 
-}
-//Função para reiniciar o jogo
-function reloadGame() {
-    secretNumber = generateSecretNumber();
-    console.log(secretNumber);
-    startMessageGame();
-    attempts = 1;
-    cleanKick();
-    document.getElementById('reload').setAttribute('disabled', true);//Com esse method(setAttribute) é necessário dar 2 parâmetros, sendo o segundo se você quer o atributo escolhido on or off.
 }
